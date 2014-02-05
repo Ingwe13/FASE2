b3d09e3ba71a32d07cc15a027a6c84a157be9a76
Javier Mariscal <>
2014-02-05 14:37:11 +0100

    ver 1.1.2

---
+++ src/pokercartas/JuegoPoker.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokercartas;
+
+/**
+ *
+ * @author usuario
+ */
+public class JuegoPoker {
+    
+    static int getCombinacion(CartaFrancesa carta1, CartaFrancesa carta2, CartaFrancesa carta3,
+                                CartaFrancesa carta4, CartaFrancesa carta5){
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    public static int NADA ;
+    public static int PAREJA;//dos cartas con el mismo numero
+    //            carta1 += carta2 + carta3 + carta4 + carta5;
+    public static int TRIO;//tres cartas con el mismo numero
+    public static int POKER;//cuatro cartas iguales
+    public static int REPOKER; 
+            
+            //carta= BarajaFrancesa.NUM_CARTAS + BarajaFrancesa.NUM_CARTAS +BarajaFrancesa.NUM_CARTAS;
+
+    
+    
+
+}
